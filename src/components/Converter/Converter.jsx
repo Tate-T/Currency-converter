c79d164f749393d60e8c486cdfeb5195f58eb148
Tate-T <tatjana.tarasovych@gmail.com>
2022-06-12 18:37:07 +0300

    handleInputChange

--- src/components/Converter/Converter.jsx
+++ src/components/Converter/Converter.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import s from './Converter.module.css';
+import rates from '../../data.json';
 
-const Converter = currency => {
+const Converter = () => {
+  const [currencySum, setcurrencySum] = useState('');
+
+  const getAmount = () => {
+    console.log('a');
+  };
+
+  const handleInputChange = e => {
+    const { value } = e.currentTarget;
+    setcurrencySum(value);
+  };
+
+  const onSubmit = e => {
+    e.preventDefault();
+  };
+
   return (
-    <form className={s.currencyForm}>
+    <form className={s.currencyForm} onClick={onSubmit}>
       <div className={s.inputStyles}>
         <div className={s.marginInput}>
           <h2>Select type of currency:</h2>
@@ -14,7 +30,13 @@ const Converter = currency => {
           </select>
         </div>
         <h2>Enter the amount:</h2>
-        <input type="text" className={s.inputCurrency} />
+        <input
+          type="text"
+          className={s.inputCurrency}
+          name="currencySum"
+          value={currencySum}
+          onChange={handleInputChange}
+        />
         <button className={s.currencyBtn}>Count</button>
         {/* <select name="currency_1" className={s.inputFirst}>
         <option value="1">UAH</option>
@@ -27,7 +49,7 @@ const Converter = currency => {
       <select type="text"></select> */}
       </div>
       <p className={s.currencyResult}>
-        Amount in UAH: <span className={s.currencyResultSum}>3300 &#8372;</span>
+        Amount in UAH: <span className={s.currencyResultSum}>0 &#8372;</span>
       </p>
     </form>
   );
