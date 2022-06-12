e001eabd0c696d5e95419a60aad7adb2e3b926b2
Tate-T <tatjana.tarasovych@gmail.com>
2022-06-12 15:48:29 +0300

    Privatbank Api

--- services/converterApi.js
+++ services/converterApi.js
@@ -1,5 +1,5 @@
 const getCurrencyRateApi = async () => {
-    const response = await fetch();
+    const response = await fetch('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5');
     if (response.ok) {
         return response.json();
     }
