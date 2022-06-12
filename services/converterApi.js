ade577a00a213571a36584bec64d706f39e2f31b
Tate-T <tatjana.tarasovych@gmail.com>
2022-06-12 18:09:23 +0300

    getrates render

--- services/converterApi.js
+++
@@ -1,9 +0,0 @@
-const getCurrencyRateApi = async () => {
-    const response = await fetch('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5');
-    if (response.ok) {
-        return response.json();
-    }
-    return Promise.reject(new Error('No currency rate there'));
-}
-
-export default getCurrencyRateApi;
--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
 import Header from './components/Header/Header.jsx';
 import Converter from './components/Converter/Converter.jsx';
+import getCurrencyRateApi from './services/converterApi';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [rates, setRates] = useState([]);
+
+  useEffect(() => {
+    getCurrencyRateApi().then(response => {
+      setRates(response.rates)
+    })
+  }, [])
+
   return (
     <div className="App">
-      <Header />
+      <Header rates={rates} />
       <Converter />
     </div>
   );
