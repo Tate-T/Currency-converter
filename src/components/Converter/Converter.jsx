0e17691b69546605423115829d96fce8f7b6d1e5
Tate-T <tatjana.tarasovych@gmail.com>
2022-06-12 15:04:55 +0300

    converter styles

--- src/components/Converter/Converter.jsx
+++ src/components/Converter/Converter.jsx
@@ -4,21 +4,30 @@ import s from "./Converter.module.css";
 const Converter = (currency) => {
   return (
     <form className={s.currencyForm}>
-      <h2>Select type of currency:</h2>
-      <select name="currency_1" className={s.inputFirst}>
+      <div className={s.inputStyles}>
+        <div className={s.marginInput}>
+          <h2>Select type of currency:</h2>
+          <select name="currency_2" className={s.inputCurrency}>
+            {/* <option value="1">UAH</option> */}
+            <option value="2">USD</option>
+            <option value="3">EUR</option>
+          </select>
+        </div>
+        <h2>Enter the amount:</h2>
+        <input type="text" className={s.inputCurrency} />
+        {/* <select name="currency_1" className={s.inputFirst}>
         <option value="1">UAH</option>
         <option value="2">USD</option>
         <option value="3">EUR</option>
-      </select>
-      <select name="currency_2" className={s.inputCurrency}>
-        <option value="1">UAH</option>
-        <option value="2">USD</option>
-        <option value="3">EUR</option>
-      </select>
-      {/* <input type="text" name="currency" list="currency" autoComplete="off" />
+      </select> */}
+
+        {/* <input type="text" name="currency" list="currency" autoComplete="off" />
       <datalist id="currency">currency</datalist>
       <select type="text"></select> */}
-      <p className={s.currencyResult}>Result: UAH</p>
+      </div>
+      <p className={s.currencyResult}>
+        Amount in UAH: <span className={s.currencyResultSum}>3300 &#8372;</span>
+      </p>
     </form>
   );
 };
